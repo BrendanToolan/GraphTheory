444d67b53582e7eb79be7ed0b34f141c6134a0c6
Brendan Toolan <btoolan73@gmail.com>
2019-03-30 23:47:10 +0000

    Thompson Constructor code add, Error in nfa

--- pythonProject.py
+++ pythonProject.py
@@ -1,3 +1,68 @@
+#Graph Theory Project - Brendan Toolan - G00350190
+
+class state:
+    label = None
+    edge1 = None
+    edge2 = None
+
+class nfa:
+    initial = None
+    accept = None
+
+    def _init_(self, initial, accept):
+        self.initial = initial
+        self.accept = accept
+
+def compile(pointFix):
+    nfastack =[]
+
+    for x in pointFix:
+        if x == '.':
+            nfa2 = nfastack.pop()
+            nfa1 = nfastack.pop()
+            nfa1.accept.edge1 = nfa2.initial
+            newnfa =  nfa(nfa1.initial, nfa2.accept)
+            nfastack.append(newnfa)
+
+        elif x == '|':
+            nfa2 = nfastack.pop()
+            nfa1 = nfastack.pop()
+           
+            initial = state()
+            initial.edge1 = nfa1.initial
+            initial.ege2 = nfa2.initial
+
+            accept = state()
+            nfa1.accept.edge1 = accept
+            nfa2.accept.edge1 = accept
+           
+            newnfa =  nfa(nfa1.initial, nfa2.accept)
+            nfastack.append(newnfa)
+
+        elif x == '*':
+            nfa1 = nfastack.pop()
+            initial = state()
+            accept = state()
+            initial.edge1 = nfa1.initial
+            initial.edge2 = accept
+            nfa1.accept.edge1= nfa1.initial
+            nfa1.accept.edge2 = accept
+            newnfa =  nfa(nfa1.initial, nfa2.accept)
+            nfastack.append(newnfa)
+
+        else:
+            accept = state()
+            initial = state()
+            initial.label = x
+            initial.edge1 = accept
+            newnfa =  nfa(nfa1.initial, nfa2.accept)
+            nfastack.append(newnfa)
+
+    return nfastack.pop()
+    
+#print(compile("ab.cd.|"))
+#print(compile("aa.*"))
+
 def shunter(infix):
     specialChar = {'*':50, '.':40, '|':30}
     pointFix = ""
